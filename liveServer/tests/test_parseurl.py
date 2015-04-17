3b96a681109dbec63141ae0495c4cfb11b8f7eed
weifanding <weifanding@yiqiding.com>
2015-04-17 13:47:39 +0800

    new file:   tests/test_parseurl.py

---
+++ liveServer/tests/test_parseurl.py
@@ -0,0 +1,23 @@
+import urllib2, base64,sys
+import urllib,geohash
+import json,sys,struct
+
+url = "http://127.0.0.1:8000/live/parseurl"
+
+postContent = {
+				"sid" : 1001, 
+				"vid" : 1,
+				"url" : "http://v.pptv.com/show/QxRz8VmicL23QTrY.html",
+				"step": 0
+}
+postData = urllib.urlencode(postContent)
+request = urllib2.Request(url, postData)
+request.get_method = lambda: 'POST'
+request.add_header('User-Agent', 'AMAP SDK iOS Search 2.2.1')
+request.add_header('Content-Type', 'application/x-www-form-urlencoded')
+result = urllib2.urlopen(request)
+contents = result.read()
+fp.open("testResult.html", "w+")
+fp.write(contents)
+fp.close()
+print contents

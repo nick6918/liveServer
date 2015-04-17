f76d3fd5ef21014b115b3e08c48530fa17500d70
weifanding <weifanding@yiqiding.com>
2015-04-17 15:07:30 +0800

    modified:   test_parseurl.py

--- liveServer/tests/test_parseurl.py
+++ liveServer/tests/test_parseurl.py
@@ -4,20 +4,24 @@ import json,sys,struct
 
 url = "http://127.0.0.1:8000/live/parseurl"
 
-postContent = {
-				"sid" : 1001, 
-				"vid" : 1,
-				"url" : "http://v.pptv.com/show/QxRz8VmicL23QTrY.html",
-				"step": 0
-}
-postData = urllib.urlencode(postContent)
-request = urllib2.Request(url, postData)
-request.get_method = lambda: 'POST'
+# postContent = {
+# 				"sid" : 1001, 
+# 				"vid" : 1,
+# 				"url" : "http://v.pptv.com/show/QxRz8VmicL23QTrY.html",
+# 				"step": 0
+# }
+# postData = urllib.urlencode(postContent)
+# request = urllib2.Request(url, postData)
+# request.get_method = lambda: 'POST'
+ 
+url = "http://127.0.0.1:8000/live/parseurl?sid=1001&vid=1&step=0&url=http://v.pptv.com/show/QxRz8VmicL23QTrY.html"
+request = urllib2.Request(url)
+
 request.add_header('User-Agent', 'AMAP SDK iOS Search 2.2.1')
 request.add_header('Content-Type', 'application/x-www-form-urlencoded')
 result = urllib2.urlopen(request)
 contents = result.read()
-fp.open("testResult.html", "w+")
+fp=open("testResult.html", "w+")
 fp.write(contents)
 fp.close()
 print contents
